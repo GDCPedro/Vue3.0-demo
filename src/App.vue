cba2bdf79bced1d80ae688b0980497a1964f7941
guodongchao <guo19941119@icloud.com>
2020-03-15 11:12:22 +0800

    ts

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,19 @@
   import { defineComponent, ref } from 'vue'
 
   export default defineComponent({
-    setup () {
+    setup (): object {
       // const state = reactive({
       //   count: 0
       // })
 
-      const count = ref(0)
+      const count: any = ref(0)
 
-      const increment = () => {
-        console.log(count)
+      // const increment = () => {
+      //   console.log(count)
+      //   count.value++
+      // }
+
+      function increment (): void {
         count.value++
       }
 
